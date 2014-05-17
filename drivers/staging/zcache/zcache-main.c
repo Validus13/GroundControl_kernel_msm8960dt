3080c614a6eb8cfd7f0d0c5a482d2218eb22a25e
Somber73 <nytesblood@gmail.com>
2014-05-16 21:56:49 -0400

    Revert "Update zcache-main.c"
    This reverts commit 728a83363e08109b8213ecc4370fb9e592c867d6.

--- drivers/staging/zcache/zcache-main.c
+++ drivers/staging/zcache/zcache-main.c
@@ -33,6 +33,8 @@
 #include <linux/string.h>
 #include "tmem.h"
 
+#include "../zsmalloc/zsmalloc.h"
+
 #if (!defined(CONFIG_CLEANCACHE) && !defined(CONFIG_FRONTSWAP))
 #error "zcache is useless without CONFIG_CLEANCACHE or CONFIG_FRONTSWAP"
 #endif
